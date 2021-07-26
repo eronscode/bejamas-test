da7efe6b6a2c9f8f4ef46c3dc50321b3dc678459
eronscode <eronscode@gmail.com>
2021-07-26 10:00:15 +0100

    Deployment

--- components/Product/ProductCard/index.js
+++ components/Product/ProductCard/index.js
@@ -4,7 +4,8 @@ import Image from "next/image";
 import { ProductCardContainer } from "./styles";
 
 function ProductCard(props) {
-  const { bestseller, price, currency, category, image, name, addToCart } = props;
+  const { bestseller, price, currency, category, image, name, addToCart } =
+    props;
   return (
     <ProductCardContainer>
       <div className="">
@@ -15,15 +16,22 @@ function ProductCard(props) {
             layout="fill"
             objectFit="cover"
             objectPosition="top center"
+            placeholder="blur"
+            blurDataURL="/images/blur.jpg"
             alt={image.alt}
           />
           {bestseller && <span className="tag-label">Best Seller</span>}
-          <Button className="cart-button" onClick={addToCart}>add to cart</Button>
+          <Button className="cart-button" onClick={addToCart}>
+            add to cart
+          </Button>
         </div>
         <div className="product-info">
           <span>{category}</span>
           <h4>{name}</h4>
-          <p>{currencyTransform(currency)}{price} </p>
+          <p>
+            {currencyTransform(currency)}
+            {price}{" "}
+          </p>
         </div>
       </div>
     </ProductCardContainer>
